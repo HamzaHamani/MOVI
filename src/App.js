edbd37737d3ba069717774e4b418ce7b946c9679
HamzaHamani <hamzahamani2004@gmail.com>
2023-10-03 14:29:57 +0100

    adding some coments to understand the code

--- src/App.js
+++ src/App.js
@@ -60,14 +60,14 @@ export default function App() {
 
   async function Fetch() {
     try {
-      setIsLoading(true); // we make loading true at the begining
-      setErrorMessage(""); // we restart error to initial value
+      setIsLoading(true); //? we make loading true at the begining
+      setErrorMessage(""); //? we restart error to initial value after loadijng
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=dfbee097&s=${query}`
       );
 
       if (!res.ok)
-        // if !res.ok or res.status !==200 we gonna throw new error
+        //! if !res.ok or res.status !==200 we gonna throw new error
         throw new Error("We couldnt fetch the data, CHECK YOUR CONNECTION");
 
       const data = await res.json();
@@ -76,25 +76,25 @@ export default function App() {
         // if user searched for not a movie
         throw new Error("we couldnt find ur movie");
       }
-      if (res.ok) setMoives(data.Search); // f res.ok or res.status ===200 we gotta setMovie to the data (always gotta be at the end of try block)
+      if (res.ok) setMoives(data.Search); //! if res.ok or res.status ===200 we gotta setMovie to the data (always gotta be at the end of try block)
     } catch (err) {
-      // here we set error message to error that we throwed
+      //? here we set error message to error that we throwed
       setErrorMessage(err.message);
     } finally {
-      setIsLoading(false); //we make loading false so data can show up after everything
+      setIsLoading(false); //!we make loading false so data can show up after everything
     }
 
-    // to check if length is small we dont need to render anything that what empty return for
+    //? to check if length is small we dont need to render anything that what empty return for
     if (query.length < 3) {
       setMoives([]); // reset movie list
-      setErrorMessage(""); //reset error
+      setErrorMessage(""); //reset error if input smaller than 3
       return; // so function wont execute
     }
   }
 
   useEffect(() => {
     Fetch();
-  }, [query]);
+  }, [query]); //? we call useeffect when querry states update on change
 
   return (
     <>
