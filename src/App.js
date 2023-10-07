cb2e55f52a83fa8ff902e9a4e94cb4888be4d58a
HamzaHamani <hamzahamani2004@gmail.com>
2023-10-07 17:10:15 +0100

    fixed number rounding issue

--- src/App.js
+++ src/App.js
@@ -399,15 +399,15 @@ function WatchedSummary({ watched }) {
         </p>
         <p>
           <span>⭐️</span>
-          <span>{avgImdbRating}</span>
+          <span>{+avgImdbRating.toFixed(2)}</span>
         </p>
         <p>
           <span>🌟</span>
-          <span>{avgUserRating}</span>
+          <span>{+avgUserRating.toFixed(2)}</span>
         </p>
         <p>
           <span>⏳</span>
-          <span>{avgRuntime} min</span>
+          <span>{avgRuntime.toFixed(2)} min</span>
         </p>
       </div>
     </div>
